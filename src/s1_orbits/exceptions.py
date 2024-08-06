441ed6fe5fb1236434019337e85dd3073007ca4e
Andrew Player <andrewplayer3@gmail.com>
2024-08-06 15:29:29 -0500

    ruff

--- src/s1_orbits/exceptions.py
+++ src/s1_orbits/exceptions.py
@@ -10,7 +10,9 @@ class InvalidSceneError(Exception):
 class OrbitNotFoundError(Exception):
     def __init__(self, scene: str):
         self.scene = scene
-        self.message = f"No orbit file could be found for the provided Sentinel-1 scene: {scene}"
+        self.message = (
+            f"No orbit file could be found for the provided Sentinel-1 scene: {scene}"
+        )
 
     def __str__(self):
         return str(self.message)
