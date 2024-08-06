129fa68a0e2eeacf7c4b883051c28cf7bae6cb54
Andrew Player <andrewplayer3@gmail.com>
2024-08-06 15:22:40 -0500

    remove unnecessary exception options

--- src/s1_orbits/exceptions.py
+++ src/s1_orbits/exceptions.py
@@ -1,24 +1,16 @@
 class InvalidSceneError(Exception):
-    def __init__(self, scene: str = ""):
-        if scene:
-            self.scene = scene
-            self.message = f"{self.scene} is an invalid scene name."
-        else:
-            self.message = "The provided scene name is invalid."
+    def __init__(self, scene: str):
+        self.scene = scene
+        self.message = f"{scene} is an invalid scene name."
 
     def __str__(self):
         return str(self.message)
 
 
 class OrbitNotFoundError(Exception):
-    def __init__(self, scene: str = ""):
-        if scene:
-            self.scene = scene
-            self.message = (
-                f"No orbit file could be found for the provided scene: {self.scene}"
-            )
-        else:
-            self.message = "No orbit file could be found for the provided scene."
+    def __init__(self, scene: str):
+        self.scene = scene
+        self.message = f"No orbit file could be found for the provided scene: {scene}"
 
     def __str__(self):
         return str(self.message)
