27d0ccfac021a83d820d0d78b368652b21d761cd
Andrew Player <andrewplayer3@gmail.com>
2024-08-06 15:22:50 -0500

    refactoring

--- tests/test_s1_orbits.py
+++ tests/test_s1_orbits.py
@@ -1,4 +1,3 @@
-import tempfile
 import responses
 import pytest
 from pathlib import Path
@@ -7,12 +6,12 @@ import s1_orbits
 
 
 @responses.activate
-def test_fetch_for_scene():
+def test_fetch_for_scene(tmp_path):
     granule = "foo"
     filename = "filename.txt"
+    file_contents = "This is some text."
     request_url = f"{s1_orbits.API_URL}/{granule}"
     response_url = f"https://bar.com/{filename}"
-    directory = tempfile.gettempdir()
 
     responses.add(
         responses.GET, request_url, status=302, headers={"Location": response_url}
@@ -21,16 +20,17 @@ def test_fetch_for_scene():
         responses.GET,
         response_url,
         status=200,
-        body="This is some text.",
+        body=file_contents,
         content_type="text/plain",
     )
-    filepath = s1_orbits.fetch_for_scene("foo", directory)
-    assert filepath == Path(directory) / filename
+    filepath = s1_orbits.fetch_for_scene("foo", tmp_path)
+    assert filepath == Path(tmp_path) / filename
+    assert filepath.read_text() == file_contents
 
     responses.add(responses.GET, request_url, status=400)
     with pytest.raises(s1_orbits.InvalidSceneError):
-        s1_orbits.fetch_for_scene("foo", directory)
+        s1_orbits.fetch_for_scene("foo", tmp_path)
 
     responses.add(responses.GET, request_url, status=404)
     with pytest.raises(s1_orbits.OrbitNotFoundError):
-        s1_orbits.fetch_for_scene("foo", directory)
+        s1_orbits.fetch_for_scene("foo", tmp_path)
