61d5662912c7c10e2e6f2af246fe53350403d05c
Andrew Player <andrewplayer3@gmail.com>
2024-08-06 15:25:52 -0500

    added sentinel 1

--- src/s1_orbits/exceptions.py
+++ src/s1_orbits/exceptions.py
@@ -1,7 +1,7 @@
 class InvalidSceneError(Exception):
     def __init__(self, scene: str):
         self.scene = scene
-        self.message = f"{scene} is an invalid scene name."
+        self.message = f"{scene} is an invalid Sentinel-1 scene name."
 
     def __str__(self):
         return str(self.message)
@@ -10,7 +10,7 @@ class InvalidSceneError(Exception):
 class OrbitNotFoundError(Exception):
     def __init__(self, scene: str):
         self.scene = scene
-        self.message = f"No orbit file could be found for the provided scene: {scene}"
+        self.message = f"No orbit file could be found for the provided Sentinel-1 scene: {scene}"
 
     def __str__(self):
         return str(self.message)
