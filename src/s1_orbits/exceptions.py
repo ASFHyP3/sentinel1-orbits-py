d3ebaa935dc047552f952ad503744c2d5eda6b51
Andrew Player <andrewplayer3@gmail.com>
2024-08-06 15:31:29 -0500

    changed wording

--- src/s1_orbits/exceptions.py
+++ src/s1_orbits/exceptions.py
@@ -1,7 +1,7 @@
 class InvalidSceneError(Exception):
     def __init__(self, scene: str):
         self.scene = scene
-        self.message = f"{scene} is an invalid Sentinel-1 scene name."
+        self.message = f"{scene} is not a valid Sentinel-1 scene name."
 
     def __str__(self):
         return str(self.message)
