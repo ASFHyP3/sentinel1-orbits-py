c7799bde16aab39b4cc7c8846f1677d6822e8982
Jake Herrmann <jtherrmann@alaska.edu>
2025-07-14 17:29:04 -0800

    uncomment ruff extensions

--- src/s1_orbits/exceptions.py
+++ src/s1_orbits/exceptions.py
@@ -1,16 +1,23 @@
+"""Custom exception types."""
+
+
 class InvalidSceneError(Exception):
-    def __init__(self, scene: str):
+    """Error for invalid scene."""
+
+    def __init__(self, scene: str) -> None:  # noqa: D107
         self.scene = scene
         self.message = f'{scene} is not a valid Sentinel-1 scene name.'
 
-    def __str__(self):
+    def __str__(self) -> str:  # noqa: D105
         return str(self.message)
 
 
 class OrbitNotFoundError(Exception):
-    def __init__(self, scene: str):
+    """Error for orbit not found."""
+
+    def __init__(self, scene: str) -> None:  # noqa: D107
         self.scene = scene
         self.message = f'No orbit file could be found for the provided Sentinel-1 scene: {scene}'
 
-    def __str__(self):
+    def __str__(self) -> str:  # noqa: D105
         return str(self.message)
